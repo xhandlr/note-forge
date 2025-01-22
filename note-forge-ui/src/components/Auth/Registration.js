eb10e40320c856e0cc3e81cc47cae24a058696b9
Camille <c.elgueta04@ufromail.cl>
2025-01-22 20:08:45 -0300

    chore: agregar navegación entre componentes
    - Agregar librería 'react-router-dom' para navegar entre los componentes
      'Login' y 'Registration'.

---
+++ note-forge-ui/src/components/Auth/Registration.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Login.css';
+
+function Registration() {
+    return (
+        <div><p>Hola</p></div>
+    );
+}
+
+export default Registration;
--- note-forge-ui/src/index.js
+++ note-forge-ui/src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Login from './components/Auth/Login';
+import Registration from './components/Auth/Registration';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'boxicons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Registration />} /> 
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
