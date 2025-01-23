2a90ce91cc6f5824ba1643f0a16d2981b3ed00ff
Camille <c.elgueta04@ufromail.cl>
2025-01-23 03:07:14 -0300

    feat: crear componente para validación de registro

--- note-forge-ui/src/components/Auth/Registration.js
+++ note-forge-ui/src/components/Auth/Registration.js
@@ -7,12 +7,24 @@ function Registration() {
             <form action="https://google.com"></form>
             <h1>Registrarse</h1>
             <div class="input-box">
+                <input type="text" placeholder="Nombre" required></input>
+                <box-icon type='solid' name='envelope' class="i"></box-icon>
+            </div>
+            <div class="input-box">
                 <input type="text" placeholder="Correo electrónico" required></input>
                 <box-icon type='solid' name='envelope' class="i"></box-icon>
             </div>
             <div class="input-box">
                 <input type="password" placeholder='Contraseña' required></input>
                 <box-icon name='lock-alt' type='solid' class="i"></box-icon>
+            </div>
+            <div class="input-box">
+                <input type="text" placeholder="País" required></input>
+                <box-icon type='solid' name='envelope' class="i"></box-icon>
+            </div>
+            <div class="input-box">
+                <input type="text" placeholder="Rol" required></input>
+                <box-icon type='solid' name='envelope' class="i"></box-icon>
             </div>
             <div class="remember-forgot">
                 <label><input type="checkbox"></input>Recordarme</label>
