0585eb7d17357a89ac678fff28e88d3478474c4f
Camille <c.elgueta04@ufromail.cl>
2025-01-24 01:15:17 -0300

    feat: establecer conexión entre mysql y backend

--- note-forge-api/index.js
+++ note-forge-api/index.js
@@ -1,35 +1,24 @@
 const express = require('express');
 const cors = require('cors');
-const mysql = require('mysql2');
+const pool = require('./config/db');
 require('dotenv').config();
-//const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-//app.use('/api/auth', authRoutes);
-
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,     
-    user: process.env.DB_USER,     
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME, 
-    port: process.env.DB_PORT,    
-});
-
+// Ruta para probar la conexión a la base de datos
 app.get('/api/test-connection', async (req, res) => {
-try {
+  try {
     const [rows] = await pool.query('SELECT 1 + 1 AS result');
     res.status(200).json({ success: true, message: 'Conexión exitosa', result: rows[0].result });
-} catch (error) {
+  } catch (error) {
     console.error('Error en la conexión:', error);
     res.status(500).json({ success: false, message: 'Error en la conexión' });
-}
+  }
 });
-  
-const PORT = 5000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
-})
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
