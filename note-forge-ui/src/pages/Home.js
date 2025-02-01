28f3703dc30480e749e98164b5d6d07affa1bc46
Camille <c.elgueta04@ufromail.cl>
2025-02-01 20:03:01 -0300

    style: crear botones para el menú con animación y sonidos
    - Crear botones CRUD para ejercicios almacenados por el usuario.
    - Agregar animaciones simples para los botones en 'Home.css'.
    - Agregar sonidos al momento de interactuar con los botones.

--- note-forge-ui/src/pages/Home.js
+++ note-forge-ui/src/pages/Home.js
@@ -1,30 +1,68 @@
-import React from "react"; // Importa biblioteca de React para realizar funciones de la misma
-import '../styles/Home.css'; // Importa un archivo .css de la carpeta styles
+import React, { useRef, useState } from "react"; 
+import '../styles/Home.css'; 
+import hoverSound from "../assets/hover-sound.wav"; 
+import menuSound from "../assets/click-sound.wav"; 
 
-function Home() { // Define una función llamada Home
+function Home() { 
+    const [soundEnabled, setSoundEnabled] = useState(false);  // Estado para controlar si los sonidos pueden reproducirse
+    const audioRef = useRef(new Audio(hoverSound));
+    const menuRef = useRef(new Audio(menuSound));
+
+    const enableSounds = () => {
+        setSoundEnabled(true);  // Permitir la reproducción de sonidos
+    };
+
+    const playSound = () => {
+        if (soundEnabled) {
+            audioRef.current.currentTime = 0; // Reinicia el sonido si ya estaba reproduciéndose
+            audioRef.current.volume = 0.5;
+            audioRef.current.play();
+        }
+    };
+
+    const playMenuSound = () => {
+        if (soundEnabled) {
+            menuRef.current.currentTime = 0;
+            menuRef.current.play();
+            menuRef.current.volume = 0.7;
+        }
+    };
+
     return ( 
-        <div className="home-page"> {/* className en lugar de class */}
+        <div className="home-page" onClick={enableSounds}>  {/* Permitir sonidos al hacer clic en cualquier parte de la página */}
             <header className="header">
-            <a href="/" className="logo">Note Forge</a>
+                <a href="/" className="logo">Note Forge</a>
 
-            {/* Checkbox para controlar la visibilidad del menú */}
-            <input type="checkbox" id="check" />
-            <label htmlFor="check" className="icons">
-                <box-icon name="menu" id="menu-icon"></box-icon>
-                <box-icon name="x" id="close-icon"></box-icon>
-            </label>
+                <input type="checkbox" id="check" />
+                <label htmlFor="check" className="icons">
+                    <box-icon name="menu" id="menu-icon" size="40px"></box-icon>
+                    <box-icon name="x" id="close-icon" size="40px"></box-icon>
+                </label>
 
-            {/* Navbar con animación */}
-            <nav className="navbar">
-                <a href="/home" style={{ "--i": 0 }} className="home-link">Inicio</a>
-                <a href="/biblioteca" style={{ "--i": 1 }} className="home-link">Biblioteca</a>
-                <a href="/exportar" style={{ "--i": 2 }} className="home-link">Exportar</a>
-                <a href="/ayuda" style={{ "--i": 3 }} className="home-link">Ayuda</a>
-                <a href="/cuenta" style={{ "--i": 4 }} className="home-link">Cuenta</a>
-            </nav>
+                <nav className="navbar">
+                    <a href="/home" className="home-link" onClick={playMenuSound}>Inicio</a>
+                    <a href="/biblioteca" className="home-link" onClick={playMenuSound}>Biblioteca</a>
+                    <a href="/exportar" className="home-link" onClick={playMenuSound}>Exportar</a>
+                    <a href="/ayuda" className="home-link" onClick={playMenuSound}>Ayuda</a>
+                    <a href="/cuenta" className="home-link" onClick={playMenuSound}>Cuenta</a>
+                </nav>
             </header>
+            <div className="home-body">
+                <div className="button-group">
+                    <div className="top-buttons">
+                        <button className="create-button" onMouseEnter={playSound} onClick={playMenuSound}>Crear Ejercicio</button>
+                        <button className="read-button" onMouseEnter={playSound} onClick={playMenuSound}>Ver Ejercicios</button>
+                    </div>
+                    <div className="bottom-buttons">
+                        <button className="delete-button" onMouseEnter={playSound} onClick={playMenuSound}>Eliminar Ejercicios</button>
+                        <button className="edit-button" onMouseEnter={playSound} onClick={playMenuSound}>Editar Ejercicios</button>
+                    </div>
+                </div>
+
+                <button className="export-button" onMouseEnter={playSound} onClick={playMenuSound}>Exportar a LaTeX</button>
+            </div>
         </div>
     );
 }
-// Este tipo de funciones se llaman componentes funcionales
-export default Home; // Exporta el componente Home para que pueda ser importado y utilizado en otros archivos
+
+export default Home;
