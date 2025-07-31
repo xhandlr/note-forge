0a6b8cfece3a13a81ac5f3b1dbd44c0c95cb9bcb
xhandlr <c.elgueta04@ufromail.cl>
2025-07-30 23:58:18 -0400

    style: agregar temas personalizados en index.css

--- note-forge-ui/src/index.jsx
+++ note-forge-ui/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './output.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'boxicons';
 
