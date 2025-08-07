41d6dc02a7f67ef076a7267a3d2e4dc8be6a48d5
xhandlr <c.elgueta04@ufromail.cl>
2025-08-06 23:01:03 -0400

    fix: corregir warnings de build de Vite

--- note-forge-ui/src/components/UI/BgDecoration.jsx
+++ note-forge-ui/src/components/UI/BgDecoration.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 function BgDecoration({file, position = "top-0 left-0",size = "w-xs h-xs", ...props}) {
-    
-    const images = import.meta.glob('/src/assets/*', { eager: true, as: 'url' });
+
+    const images = import.meta.glob('/src/assets/*', { eager: true, query: '?url', import: 'default' });
     const src = images[`/src/assets/${file}`];
 
     const base = "absolute mt-0 mr-0 pointer-events-none select-none"
