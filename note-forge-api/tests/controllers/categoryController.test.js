0655c38670474a4970e6c313bcafc5754c6d1e00
xhandlr <c.elgueta04@ufromail.cl>
2025-08-21 12:33:40 -0400

    test: crear tests principales para controller de categorias (#12)
    - Test para obtener todas la categorías de un usuario
    - Test para actualizar una categoría
    - Test para eliminar una categoría

--- note-forge-api/tests/controllers/categoryController.test.js
+++ note-forge-api/tests/controllers/categoryController.test.js
@@ -41,6 +41,10 @@ describe('Category Controller', () => {
         });
     });
 
+    /**
+     * Test for get category by id
+     * Endpoint GET /category/:id
+     */
     describe('GET /category/:id', () => {
         let token;
         let categoryId;
@@ -58,6 +62,76 @@ describe('Category Controller', () => {
             expect(res.statusCode).toBe(200);
             expect(res.body).toHaveProperty('name');
             expect(res.body).toHaveProperty('description');
+        });
+    });
+
+    /**
+     * Test for get all the user categories
+     * Endpoint GET /categories
+     */
+    describe('GET /categories', () => {
+        beforeAll(async () => {
+            token = await createTestUserAndLogin();
+            categoryId = await createTestCategory(token);
+        });
+
+        it('should return all the user categories', async () => {
+            const res = await request(app)
+            .get('/categories')
+            .set('Authorization', `Bearer ${token}`);
+
+            expect(res.statusCode).toBe(200);
+            expect(Array.isArray(res.body)).toBe(true);
+        })
+    });
+
+    /**
+     * Test for updating a category
+     * Endpoint PUT /update-category/:id
+     */
+    describe('PUT /update-category/:id', () => {
+        let token;
+        let categoryId;
+
+        beforeAll(async () => {
+            token = await createTestUserAndLogin();
+            categoryId = await createTestCategory(token);
+        });
+
+        it('should update the category', async () => {
+            const res = await request(app)
+            .put(`/update-category/${categoryId}`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                name: 'Updated Category',
+                description: 'This is an updated test category',
+                isPinned: true
+            });
+
+            expect(res.statusCode).toBe(200);
+        });
+    });
+
+    /**
+     * Test for deleting a category
+     * Endpoint DELETE /delete-category/:id
+     */
+    describe('DELETE /delete-category/:id', () => {
+        let token;
+        let categoryId;
+
+        beforeAll(async () => {
+            token = await createTestUserAndLogin();
+            categoryId = await createTestCategory(token);
+        });
+
+        it('should delete the category', async () => {
+            const res = await request(app)
+            .delete(`/delete-category/${categoryId}`)
+            .set('Authorization', `Bearer ${token}`);
+
+            expect(res.statusCode).toBe(200);
+            expect(res.body).toHaveProperty('message', 'Categoría eliminada con éxito');
         });
     });
 });
